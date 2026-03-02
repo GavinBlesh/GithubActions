273e059e849d7a432fa52231704014533a90f763
GavinBlesh <bleshgavin@gmail.com>
2026-03-02 17:33:18 -0600

    added test cases

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -26,4 +26,100 @@ public class Addition
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, "1"));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
 	}
+
+	[TestMethod]
+	public void Subtract_Valid_Patino_Blesh()
+	{
+		Asset.AreEqual(10, Program.Subtract("12", "2"));
+		Assert.AreEqual(1, Program.Subtract("3", "2"));
+		Assert.AreEqual(253, Program.Subtract("490", "237"));
+    }
+
+	[TestMethod]
+	public void Subtract_Invalid_Patino_Blesh()
+	{
+		Assert.ThrowsException<FormatException>(() => Program.Subtract("2", "z"));
+		Assert.ThrowsException<FormatException>(() => Program.Subtract("z", "2"));
+		Assert.ThrowsException<FormatException>(() => Program.Subtract("z", "z"));
+    }
+
+	[TestMethod]
+	public void Subtract_Null_Patino_Blesh()
+	{
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract("5", null));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, "5"));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, null));
+    }
+
+    [TestMethod]
+    public void Multiply_Valid_Patino_Blesh()
+	{
+		Assert.AreEqual(10, Program.Multiply("5", "2"));
+		Assert.AreEqual(6, Program.Multiply("3", "2"));
+		Assert.AreEqual(1158, Program.Multiply("490", "237"));
+    }
+
+    [TestMethod]
+    public void Multiply_Invalid_Patino_Blesh()
+	{
+		Assert.ThrowsException<FormatException>(() => Program.Multiply("2", "z"));
+		Assert.ThrowsException<FormatException>(() => Program.Multiply("z", "2"));
+		Assert.ThrowsException<FormatException>(() => Program.Multiply("z", "z"));
+    }
+
+    [TestMethod]
+    public void Multiply_Null_Patino_Blesh()
+	{
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply("5", null));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, "5"));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, null));
+    }
+
+    [TestMethod]
+	public void Divide_Valid_Patino_Blesh()
+	{
+		Assert.AreEqual(6, Program.Divide("10", "2")); //Intentionally Wrong
+		Assert.AreEqual(1.5, Program.Divide("3", "2"));
+		Assert.AreEqual(2.07, Program.Divide("490", "237"), 0.01);
+	}
+	
+	[TestMethod]
+	public void Divide_Invalid_Patino_Blesh()
+	{
+		Assert.ThrowsException<FormatException>(() => Program.Divide("2", "z"));
+		Assert.ThrowsException<FormatException>(() => Program.Divide("z", "2"));
+		Assert.ThrowsException<FormatException>(() => Program.Divide("z", "z"));
+	}
+	
+	[TestMethod]
+	public void Divide_Null_Patino_Blesh()
+	{
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Divide("5", null));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, "5"));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, null));
+    }
+
+    [TestMethod]
+	public void Power_Valid_Patino_Blesh()
+	{
+	Assert.AreEqual(100, Program.Power("10", "2"));
+	Assert.AreEqual(4, Program.Power("2", "2"));
+	Assert.AreEqual(8, Program.Power("2", "3"));
+    }
+
+    [TestMethod]
+	public void Power_Invalid_Patino_Blesh()
+	{
+		Assert.ThrowsException<FormatException>(() => Program.Power("2", "z"));
+		Assert.ThrowsException<FormatException>(() => Program.Power("z", "2"));
+		Assert.ThrowsException<FormatException>(() => Program.Power("z", "z"));
+    }
+	
+	[TestMethod]
+	public void Power_Null_Patino_Blesh()
+	{
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Power("5", null));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, "5"));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, null));
+    }
 }
